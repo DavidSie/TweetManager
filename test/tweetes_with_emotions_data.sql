ae43b5b220427a5b12f4063f4135804aa7a94b0c
David Sieciński <david.siecinski@goldenoakit.pl>
2024-07-12 14:33:29 +0200

    Adds Test data for tweets with emotions

---
+++ test/tweetes_with_emotions_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tweets_with_emotions (id, text, author_id, symbol, tweet_created_at, created_at, updated_at)
+VALUES 
+    (1,'@SOMEONE So True :)', 'someone1','mysymbol1','2024-06-01','2024-07-12','2024-07-12'), 
+    (2,'@SOMEONE @SOMEONE ...Catherina, I know what u meant, but I love the wsy u worded the last sentence :)', 'someone2','mysymbol1','2024-06-01','2024-07-12','2024-07-12'),
+    (3,'Please untag me - I have blocked her for a reason. Thanks :) @SOMEONE @SOMEONE @SOMEONE @SOMEONE', 'someone3','mysymbol1','2024-06-02','2024-07-12','2024-07-12'),
+    (4,'Thank you so much @SOMEONE_ , @SOMEONE , @SOMEONE and @SOMEONE for following me :-) Much appreciated ;-)', 'somone4','mysymbol1','2024-06-03','2024-07-12','2024-07-12'),
+    (5,'i love writing little notes on my receipts to my servers when they do a great job :-)', 'someone4','mysymbol3','2024-06-04','2024-07-12','2024-07-12'),
+    (6,'RT @SOMEONE: why does no one ever listen to harry potter....he was right about voldemort coming back for 7 books and every year they re lik…','someone5','mysymbol3','2024-06-04','2024-07-12','2024-07-12'),
+    (7,'RT @SOMEONE: I just wanna be good enough :((' ,'someone6','mysymbol3','2024-06-04','2024-07-12','2024-07-12'),
+    (8,'RT @SOMEONE: #sigh More PC stuff to sort out :( Should be sorted soon)' ,'someone7','mysymbol3','2024-06-04','2024-07-12','2024-07-12');
